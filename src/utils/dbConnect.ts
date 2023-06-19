461ae4492f6e0bf6efe3b1b6f1300c20fb698810
Raj Mazumder <rajmazumder27.08.2001@gmail.com>
2023-06-19 15:58:18 +0530

    URL name change

--- src/utils/dbConnect.ts
+++ src/utils/dbConnect.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectToDB = async () => {
-  const URL = process.env.MONGODB_URL;
+  const URL = process.env.MONGODB_URI;
   if (mongoose.connection.readyState !== 1) {
     await mongoose.connect(URL as string);
   }
