0fed0411947ea25c34a88f70fd1047bcb628ff9a
Raj Mazumder <rajmazumder27.08.2001@gmail.com>
2023-06-20 13:00:18 +0530

    UI Fixed market

--- src/components/client/PairView.tsx
+++ src/components/client/PairView.tsx
@@ -1,4 +1,4 @@
-import { ACTIVE_CHAIN, CoinMingleRouter, EXPLORER } from "@config";
+import { ACTIVE_CHAIN, CoinMingleRouter, EXPLORER, WFTM } from "@config";
 import { useToken, useContractRead } from "wagmi";
 import CM_ROUTER from "@abis/Router.json";
 import CM_LP from "@abis/LP.json";
@@ -54,17 +54,21 @@ const PairView: FC<ITokens> = ({ tokenA, tokenB }) => {
   );
 
   return (
-    <div className="w-full px-8 flex justify-between items-center min-h-32 py-5 bg-slate-900 bg-opacity-5 backdrop-blur-xl rounded-xl transition-all hover:border hover:border-slate-300">
+    <div className="w-full px-8 flex justify-between items-center min-h-32 py-5 bg-slate-900 bg-opacity-5 backdrop-blur-xl rounded-xl hover:border hover:border-slate-300">
       <div className="">
         <div className="flex gap-1 text-md">
           <div className="flex items-center">
             <Image src={"/ftm-logo.svg"} alt="" width={20} height={20} />
-            <h1 className="text-bold">{tokenA_data?.symbol}</h1>
+            <h1 className="text-bold">
+              {tokenA.address === WFTM ? "FTM" : tokenA_data?.symbol}
+            </h1>
           </div>
           <p>/</p>
           <div className="flex items-center">
             <Image src={"/ftm-logo.svg"} alt="" width={20} height={20} />
-            <h1 className="text-bold">{tokenB_data?.symbol}</h1>
+            <h1 className="text-bold">
+              {tokenB.address === WFTM ? "FTM" : tokenB_data?.symbol}
+            </h1>
           </div>
         </div>
         <p className="text-sm mt-1 text-white font-medium">
@@ -75,22 +79,27 @@ const PairView: FC<ITokens> = ({ tokenA, tokenB }) => {
                   perTokenOut as BigInt,
                   tokenB_data?.decimals
                 )!.toString()
-              ).toFixed(4)
+              ).toFixed(2)
             : "0"}{" "}
-          {tokenA_data?.symbol} / {tokenB_data?.symbol}
+          {tokenB.address === WFTM ? "FTM" : tokenB_data?.symbol} /{" "}
+          {tokenA.address === WFTM ? "FTM" : tokenA_data?.symbol}
         </p>
       </div>
       <div className="flex flex-col items-center">
         <p className="text-sm">Reserve</p>
         {reservesFetched && typeof reservesAmounts !== "undefined" ? (
           <p className="text-lg font-medium">
-            {Number(
+            {parseFloat(
               formatToken(
                 // @ts-ignore
-                reservesAmounts[0] as BigInt,
+                tokenA.address === WFTM
+                  ? // @ts-ignore
+                    (reservesAmounts[1] as BigInt)
+                  : // @ts-ignore
+                    (reservesAmounts[0] as BigInt),
                 tokenA_data?.decimals
-              )
-            ).toLocaleString()}
+              )!.toString()
+            ).toFixed(2)}
           </p>
         ) : (
           <p className="text-lg">0</p>
@@ -101,13 +110,16 @@ const PairView: FC<ITokens> = ({ tokenA, tokenB }) => {
           <p className="text-sm">Reserve</p>
           {reservesFetched && typeof reservesAmounts !== "undefined" ? (
             <p className="text-lg font-medium">
-              {Number(
+              {parseFloat(
                 formatToken(
-                  // @ts-ignore
-                  reservesAmounts[1] as BigInt,
+                  tokenA.address === WFTM
+                    ? // @ts-ignore
+                      (reservesAmounts[0] as BigInt)
+                    : // @ts-ignore
+                      (reservesAmounts[1] as BigInt),
                   tokenB_data?.decimals
-                )
-              ).toLocaleString()}
+                )!.toString()
+              ).toFixed(2)}
             </p>
           ) : (
             <p className="text-lg">0</p>
