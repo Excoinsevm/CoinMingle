99130d8ee825cb9200975734e90b35cd8107fdee
Raj Mazumder <rajmazumder27.08.2001@gmail.com>
2023-07-01 20:09:21 +0530

    Walletconnect v2 updated

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,18 +1,18 @@
 /// Mainnet Chain
-// import { fantom } from "wagmi/chains";
-// export const chains = [fantom];
-// export const ACTIVE_CHAIN = fantom;
-// export const EXPLORER = "https://ftmscan.com";
-// export const CoinMingleRouter = "0xdAF48C05573B92F2B50d75E9A0Db91F5B83Ca359";
-// export const WFTM = "0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83";
+import { fantom } from "wagmi/chains";
+export const chains = [fantom];
+export const ACTIVE_CHAIN = fantom;
+export const EXPLORER = "https://ftmscan.com";
+export const CoinMingleRouter = "0xdAF48C05573B92F2B50d75E9A0Db91F5B83Ca359";
+export const WFTM = "0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83";
 
 /// Testnet Chain
-import { fantomTestnet } from "wagmi/chains";
-export const chains = [fantomTestnet];
-export const ACTIVE_CHAIN = fantomTestnet;
-export const EXPLORER = "https://testnet.ftmscan.com";
-export const CoinMingleRouter = "0x6B37410133cDC3365fFd80008b6C1040Db83a04f";
-export const WFTM = "0x812666209b90344ec8e528375298ab9045c2bd08";
+// import { fantomTestnet } from "wagmi/chains";
+// export const chains = [fantomTestnet];
+// export const ACTIVE_CHAIN = fantomTestnet;
+// export const EXPLORER = "https://testnet.ftmscan.com";
+// export const CoinMingleRouter = "0x6B37410133cDC3365fFd80008b6C1040Db83a04f";
+// export const WFTM = "0x812666209b90344ec8e528375298ab9045c2bd08";
 
 /// OTHER CONFIG
 export const PROJECT_ID = "ab3bbfc86a151e76c8aac6bbaea9ccf8";
