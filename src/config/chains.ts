cf4d6b09c0fe1841ea946076925f79a7272f0f04
Excoinsevm <149539033+Excoinsevm@users.noreply.github.com>
2024-10-20 03:23:26 +0100

    Create chains.ts

---
+++ src/config/chains.ts
@@ -0,0 +1,16 @@
+export const bitrock = defineChain({
+  chain: "bitrock",
+  name: "bitrock",
+  chainId: 7171,
+  rpc: ["https://connect.bit-rock.io"],
+  nativeCurrency: {
+    name: "BROCK",
+    symbol: "BROCK",
+    decimals: 18,
+  },
+  shortName: "custom",
+  testnet: false,
+  slug: "bitrock",
+});
+
+export default bitrock;
